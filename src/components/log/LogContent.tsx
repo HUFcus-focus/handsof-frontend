cbedaac196a6d2f57a0204cc2d6ad11a60f913bb
unknown <sjjoseph106@gmail.com>
2022-07-24 08:20:55 +0900

    :bug: CORS 해결!

--- src/components/log/LogContent.tsx
+++ src/components/log/LogContent.tsx
@@ -6,13 +6,13 @@ import { useState, useEffect } from "react";
 const LogContent = () => {
   const [messageList, setMessageList] = useState([]);
 
-  /* useEffect(() => {
+  useEffect(() => {
     fetch("https://api.handsof.today/chat/slack/62dbd9ba9e804cba2f19ea16", {})
       .then((response) => response.json())
       .then((data) => {
         setMessageList(data);
       });
-  }, []); */
+  }, []);
 
   return (
     <>
@@ -21,16 +21,9 @@ const LogContent = () => {
         <TitleLine />
       </Title>
       <S.LogContentContainer>
-        {/* {messageList.map((e) => (
+        {messageList.map((e) => (
           <LogMessage key={e.id} date={e.createdAt} text={e.message} />
-        ))} */}
-        <LogMessage date={"2022.07.24 21:38"} text={"힘들 것 같아요 :("} />
-        <LogMessage date={"2022.07.23 9:16"} text={"좋아요!!"} />
-        <LogMessage date={"2022.07.23 7:41"} text={"안녕하세요!"} />
-        <LogMessage date={"2022.07.22 9:16"} text={"사랑해요 :>"} />
-        <LogMessage date={"2022.07.21 17:32"} text={"감사해요 :)"} />
-        <LogMessage date={"2022.07.24 21:38"} text={"힘들 것 같아요 :("} />
-        <LogMessage date={"2022.07.19 17:21"} text={"안녕하세요!"} />
+        ))}
       </S.LogContentContainer>
     </>
   );
